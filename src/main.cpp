9938fd6ccf5cf55369b22ca50c5e44650dd49da3
Holger Adams <mail@dm5tt.de>
2024-08-28 16:22:40 +0200

    main: set brown-out detection to 3.0v

--- src/main.cpp
+++ src/main.cpp
@@ -118,6 +118,9 @@ AudioThread *audioThread = nullptr;
 float tcxoVoltage = SX126X_DIO3_TCXO_VOLTAGE; // if TCXO is optional, put this here so it can be changed further down.
 #endif
 
+#include "soc/rtc_cntl_reg.h"
+#include "soc/soc.h"
+
 using namespace concurrency;
 
 volatile static const char slipstreamTZString[] = USERPREFS_TZ_STRING;
@@ -243,6 +246,10 @@ void printInfo()
 #ifndef PIO_UNIT_TESTING
 void setup()
 {
+
+    REG_SET_BIT(RTC_CNTL_BROWN_OUT_REG, RTC_CNTL_BROWN_OUT_ENA); // Enable brownout
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 7);                   // Set threshold to max (3.0V)
+
     concurrency::hasBeenSetup = true;
 #if ARCH_PORTDUINO
     SPISettings spiSettings(settingsMap[spiSpeed], MSBFIRST, SPI_MODE0);
